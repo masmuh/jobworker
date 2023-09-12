3e0875f32778637e794ca5e69226fba370c594f9
mas muhammad <masmuh@outlook.com>
2023-09-12 13:57:12 +0700

    Add project files.

---
+++ jobworker/Program.cs
@@ -0,0 +1,10 @@
+using jobworker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
